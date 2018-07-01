e7c7ebbb43d440b72f80ea65a0765f185d24db35
Ayappa Reddy <aayappareddy@gmail.com>
2018-07-01 16:24:00 +0530

    Added the main tags page

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -1,3 +1,43 @@
 import React from 'react';
+import { kebabCase } from 'lodash';
+import Link from 'gatsby-link';
 
-export default () => <div>Tags</div>;
+const TagsPage = ({
+  data: {
+    allMarkdownRemark: { group },
+    site: {
+      siteMetadata: { title },
+    },
+  },
+}) => (
+  <div>
+    <h1>Tags</h1>
+    <ul>
+      {group.map(tag => (
+        <li key={tag.fieldValue}>
+          <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+            {tag.fieldValue} ({tag.totalCount})
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default TagsPage;
+
+export const tagPageQuery = graphql`
+  query TagsQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(limit: 1000) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`;
