ff1caa6499ed47d3fff5d698b07e6c5ec5163d93
Ayappa Reddy <aayappareddy@gmail.com>
2018-07-01 17:42:07 +0530

    Cleaned up gitignore

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,4 +2,5 @@ import CMS from 'netlify-cms';
 
 import BlogPostPreview from './preview-templates/BlogPostPreview';
 
+CMS.registerPreviewStyle('/styles.css');
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
