e51f71838a7711cb74c5015138cefe10f65e8065
Ayappa Reddy <aayappareddy@gmail.com>
2018-07-01 13:06:02 +0530

    Added initial boilerplate for main index page  and blog-post template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
 
-export default () => <div>Hello Gatsby!</div>;
+export default class IndexPage extends Component {
+  render() {
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
+
+    return (
+      <div style={{ margin: '0 auto', maxWidth: 600 }}>
+        {posts.map(({ node: post }) => (
+          <div
+            key={post.id}
+            style={{ border: '1px solid #eee', marginBottom: 20, padding: 20 }}
+          >
+            <Link to={post.fields.slug}>
+              <h1>{post.frontmatter.title}</h1>
+            </Link>
+            <p> - {post.frontmatter.date}</p>
+            <div>
+              <p>{post.excerpt}</p>
+              <br />
+              <br />
+              <Link to={post.fields.slug}>Keep Reading</Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+};
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+            date(formatString: "MMMM DD, YYYY")
+          }
+          excerpt(pruneLength: 400)
+        }
+      }
+    }
+  }
+`;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,3 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
 
-export default () => <div>Blog Post</div>;
+const BlogPost = ({ data }) => {
+  const { markdownRemark: post } = data;
+  return (
+    <div>
+      <h1>{post.frontmatter.title}</h1>
+      <p>{post.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <Link to={'/'}>Home</Link>
+    </div>
+  );
+};
+
+BlogPost.PropTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+};
+
+export default BlogPost;
+
+export const pageQuery = graphql`
+  query BlogPostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+        tags
+      }
+      html
+    }
+  }
+`;
