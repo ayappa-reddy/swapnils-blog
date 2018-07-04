db5b9e8a946ddfc84201c0c1dd91c9e297fa091a
Ayappa Reddy <aayappareddy@gmail.com>
2018-07-04 09:35:49 +0530

    Added typography and emotion to the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,29 @@
 module.exports = {
   siteMetadata: {
-    title: `Swapnil's Blog`,
+    title: `Swapnil's Blog`
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages/`,
-        name: `pages`,
-      },
+        name: `pages`
+      }
     },
     `gatsby-transformer-remark`,
     {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`
+      }
+    },
+    `gatsby-plugin-emotion`,
+    {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
     },
-    `gatsby-plugin-netlify`, // make sure to keep it last in the array
-  ],
+    `gatsby-plugin-netlify` // make sure to keep it last in the array
+  ]
 };
